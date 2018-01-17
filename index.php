4d70015908bc6050e676acbcc3c830815f438bdd
4alit <35354743+4alit@users.noreply.github.com>
2018-01-17 00:50:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
-
 class Interview
 {
-	public $title = 'Interview test';
+	public static $title = 'Interview test';//we call static field in H1 tag 
+        /*or should create an istance of the class if it's not static and call it like: <h1><?php $obj=new Interview();echo $obj->$title;?></h1>*/
 }
-
 $lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt, quasi aliquid, quod officia commodi magni eum? Provident, sed necessitatibus perferendis nisi illum quos, incidunt sit tempora quasi, pariatur natus.';
-
 $people = array(
 	array('id'=>1, 'first_name'=>'John', 'last_name'=>'Smith', 'email'=>'john.smith@hotmail.com'),
 	array('id'=>2, 'first_name'=>'Paul', 'last_name'=>'Allen', 'email'=>'paul.allen@microsoft.com'),
@@ -14,9 +12,7 @@ $people = array(
 	array('id'=>4, 'first_name'=>'Steve', 'last_name'=>'Buscemi', 'email'=>'steve.buscemi@yahoo.com'),
 	array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
 );
-
 $person = $_POST['person'];
-
 ?>
 
 <!DOCTYPE html>
@@ -34,8 +30,8 @@ $person = $_POST['person'];
 
 	<?php
 	// Print 10 times
-	for ($i=10; $i<0; $i++) {
-		echo '<p>'+$lipsum+'</p>';
+	for ($i=0; $i<10; $i++) {//iterate from 0 to 9
+		echo '<p>'.$lipsum.'</p>';//to concatenate strings in php use '.' instead of + 
 	}
 	?>
 
@@ -43,7 +39,7 @@ $person = $_POST['person'];
 	<hr>
 
 
-	<form method="get" action="<?=$_SERVER['REQUEST_URI'];?>">
+        <form method="post" action="<?=$_SERVER['REQUEST_URI'];?>"><!--we read superglobal var $_POST in php script. Must use post method-->
 		<p><label for="firstName">First name</label> <input type="text" name="person[first_name]" id="firstName"></p>
 		<p><label for="lastName">Last name</label> <input type="text" name="person[last_name]" id="lastName"></p>
 		<p><label for="email">Email</label> <input type="text" name="person[email]" id="email"></p>
@@ -67,11 +63,11 @@ $person = $_POST['person'];
 			</tr>
 		</thead>
 		<tbody>
-			<?php foreach ($people as $person): ?>
+			<?php foreach ($people as $person): ?><!--$person is an array. Not an object with public fields first_name,last_name,email-->
 				<tr>
-					<td><?=$person->first_name;?></td>
-					<td><?=$person->last_name;?></td>
-					<td><?=$person->email;?></td>
+					<td><?=$person['first_name'];?></td>
+					<td><?=$person['last_name'];?></td>
+					<td><?=$person['email'];?></td>
 				</tr>
 			<?php endforeach; ?>
 		</tbody>
