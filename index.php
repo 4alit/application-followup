b16e53d7947b741e7526e1bc84f43e6cf9e717b6
4alit <35354743+4alit@users.noreply.github.com>
2018-01-26 17:45:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,77 +1,96 @@
 <?php
-class Interview
-{
-	public static $title = 'Interview test';//we call static field in H1 tag 
-        /*or should create an istance of the class if it's not static and call it like: <h1><?php $obj=new Interview();echo $obj->title;?></h1>*/
-}
-$lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt, quasi aliquid, quod officia commodi magni eum? Provident, sed necessitatibus perferendis nisi illum quos, incidunt sit tempora quasi, pariatur natus.';
+//I made some little changes in the code. I would make php and html in separate files and add js for the loops as well. But probable not for this simple example 
+//I do not think we need a class for the example. But we can use it like this if you want
+/* class Interview {
+
+  public function getTitle() {
+  return 'Interview test';
+  }
+
+  public function getPeople() {
+  return array(
+  array('id' => 1, 'first_name' => 'John', 'last_name' => 'Smith', 'email' => 'john.smith@hotmail.com'),
+  array('id' => 2, 'first_name' => 'Paul', 'last_name' => 'Allen', 'email' => 'paul.allen@microsoft.com'),
+  array('id' => 3, 'first_name' => 'James', 'last_name' => 'Johnston', 'email' => 'james.johnston@gmail.com'),
+  array('id' => 4, 'first_name' => 'Steve', 'last_name' => 'Buscemi', 'email' => 'steve.buscemi@yahoo.com'),
+  array('id' => 5, 'first_name' => 'Doug', 'last_name' => 'Simons', 'email' => 'doug.simons@hotmail.com')
+  );
+  }
+
+  public function getLipsum() {
+  return  $lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt, quasi aliquid, quod officia commodi magni eum? Provident, sed necessitatibus perferendis nisi illum quos, incidunt sit tempora quasi, pariatur natus.';
+  }
+  }
+
+  $obj = new Interview();
+  $title = $obj->getTitle();
+  $lipsum = $obj->getLipsum();
+  $people = $obj->getPeople(); */
+
+$title = 'Interview test';
 $people = array(
-	array('id'=>1, 'first_name'=>'John', 'last_name'=>'Smith', 'email'=>'john.smith@hotmail.com'),
-	array('id'=>2, 'first_name'=>'Paul', 'last_name'=>'Allen', 'email'=>'paul.allen@microsoft.com'),
-	array('id'=>3, 'first_name'=>'James', 'last_name'=>'Johnston', 'email'=>'james.johnston@gmail.com'),
-	array('id'=>4, 'first_name'=>'Steve', 'last_name'=>'Buscemi', 'email'=>'steve.buscemi@yahoo.com'),
-	array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
+    array('id' => 1, 'first_name' => 'John', 'last_name' => 'Smith', 'email' => 'john.smith@hotmail.com'),
+    array('id' => 2, 'first_name' => 'Paul', 'last_name' => 'Allen', 'email' => 'paul.allen@microsoft.com'),
+    array('id' => 3, 'first_name' => 'James', 'last_name' => 'Johnston', 'email' => 'james.johnston@gmail.com'),
+    array('id' => 4, 'first_name' => 'Steve', 'last_name' => 'Buscemi', 'email' => 'steve.buscemi@yahoo.com'),
+    array('id' => 5, 'first_name' => 'Doug', 'last_name' => 'Simons', 'email' => 'doug.simons@hotmail.com')
 );
-$person = $_POST['person'];
+$lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt, quasi aliquid, quod officia commodi magni eum? Provident, sed necessitatibus perferendis nisi illum quos, incidunt sit tempora quasi, pariatur natus.';
+//I moved the if statement from html to here. I am not sure it's good solution. but for me it looks better
+$personToSubmit = 'Imput the data!';
+if ($_POST) {
+    if (!empty(implode($_POST))) {
+        $personToSubmit = implode(' ', $_POST);
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Interview test</title>
-	<style>
-		body {font: normal 14px/1.5 sans-serif;}
-	</style>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Interview test</title>
+        <style>
+            body {font: normal 14px/1.5 sans-serif;}
+        </style>
+    </head>
+    <body>
 
-	<h1><?=Interview::$title;?></h1>
+        <h1><?php echo $title; ?></h1>
+        <?php
+        for ($i = 0; $i < 10; $i++) {
+            echo '<p>' . $lipsum . '</p>';
+        }
+        ?>
 
-	<?php
-	// Print 10 times
-	for ($i=0; $i<10; $i++) {//iterate from 0 to 9
-		echo '<p>'.$lipsum.'</p>';//to concatenate strings in php use '.' instead of + 
-	}
-	?>
+        <hr>
+        <form method="post" action=""><!--I removed $_SERVER['REQUEST_URI']. we do not need it here and it's not safe-->
+            <p><label for="firstName">First name</label> <input type="text" name="first_name" id="firstName"></p>
+            <p><label for="lastName">Last name</label> <input type="text" name="last_name" id="lastName"></p>
+            <p><label for="email">Email</label> <input type="text" name="email" id="email"></p>
+            <p><input type="submit" value="Submit" /></p>
+        </form>
+        <p><strong>Person</strong> <?php echo $personToSubmit; ?></p>
+        <hr>
 
+        <table>
+            <thead>
+                <tr>
+                    <th>First name</th>
+                    <th>Last name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($people as $person){ ?>
+                    <tr>
+                        <td><?php echo $person['first_name']; ?></td>
+                        <td><?php echo $person['last_name']; ?></td>
+                        <td><?php echo $person['email']; ?></td>
+                    </tr>
+                <?php }; ?>
+            </tbody>
+        </table>
 
-	<hr>
-
-
-        <form method="post" action="<?=$_SERVER['REQUEST_URI'];?>"><!--we read superglobal var $_POST in php script. Must use post method-->
-		<p><label for="firstName">First name</label> <input type="text" name="person[first_name]" id="firstName"></p>
-		<p><label for="lastName">Last name</label> <input type="text" name="person[last_name]" id="lastName"></p>
-		<p><label for="email">Email</label> <input type="text" name="person[email]" id="email"></p>
-		<p><input type="submit" value="Submit" /></p>
-	</form>
-
-	<?php if ($person): ?>
-		<p><strong>Person</strong> <?=$person['first_name'];?>, <?=$person['last_name'];?>, <?=$person['email'];?></p>
-	<?php endif; ?>
-
-
-	<hr>
-
-
-	<table>
-		<thead>
-			<tr>
-				<th>First name</th>
-				<th>Last name</th>
-				<th>Email</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ($people as $person): ?><!--$person is an array. Not an object with public fields first_name,last_name,email-->
-				<tr>
-					<td><?=$person['first_name'];?></td>
-					<td><?=$person['last_name'];?></td>
-					<td><?=$person['email'];?></td>
-				</tr>
-			<?php endforeach; ?>
-		</tbody>
-	</table>
-
-</body>
+    </body>
 </html>
