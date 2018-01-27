4a1d294b8eee2d537fd8dd343606becd785a2f05
4alit <35354743+4alit@users.noreply.github.com>
2018-01-26 22:32:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,29 +3,42 @@
 //I do not think we need a class for the example. But we can use it like this if you want
 /* class Interview {
 
-  public function getTitle() {
-  return 'Interview test';
-  }
-
-  public function getPeople() {
-  return array(
-  array('id' => 1, 'first_name' => 'John', 'last_name' => 'Smith', 'email' => 'john.smith@hotmail.com'),
-  array('id' => 2, 'first_name' => 'Paul', 'last_name' => 'Allen', 'email' => 'paul.allen@microsoft.com'),
-  array('id' => 3, 'first_name' => 'James', 'last_name' => 'Johnston', 'email' => 'james.johnston@gmail.com'),
-  array('id' => 4, 'first_name' => 'Steve', 'last_name' => 'Buscemi', 'email' => 'steve.buscemi@yahoo.com'),
-  array('id' => 5, 'first_name' => 'Doug', 'last_name' => 'Simons', 'email' => 'doug.simons@hotmail.com')
+  protected $title = 'Interview test';
+  protected $people = array(
+                    array('id' => 1, 'first_name' => 'John', 'last_name' => 'Smith', 'email' => 'john.smith@hotmail.com'),
+                    array('id' => 2, 'first_name' => 'Paul', 'last_name' => 'Allen', 'email' => 'paul.allen@microsoft.com'),
+                    array('id' => 3, 'first_name' => 'James', 'last_name' => 'Johnston', 'email' => 'james.johnston@gmail.com'),
+                    array('id' => 4, 'first_name' => 'Steve', 'last_name' => 'Buscemi', 'email' => 'steve.buscemi@yahoo.com'),
+                    array('id' => 5, 'first_name' => 'Doug', 'last_name' => 'Simons', 'email' => 'doug.simons@hotmail.com')
   );
+  protected $lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt, quasi aliquid, quod officia commodi magni eum? Provident, sed necessitatibus perferendis nisi illum quos, incidunt sit tempora quasi, pariatur natus.';
+
+  public function __get($property) {
+    if (isset($property)) {
+        return $this->$property;
+    }
   }
 
-  public function getLipsum() {
-  return  $lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt, quasi aliquid, quod officia commodi magni eum? Provident, sed necessitatibus perferendis nisi illum quos, incidunt sit tempora quasi, pariatur natus.';
-  }
+  /**
+ * @param array $post
+ * @return string
+ */
+/*  public function getPersonToSubmit($post) {
+        if ($post) {
+            if (!empty(implode($post))) {
+                 return implode(' ', $post);
+            }
+            return 'Imput the data!';
+        }
+        return 'Did not get $_POST.';
+    }
   }
 
   $obj = new Interview();
-  $title = $obj->getTitle();
-  $lipsum = $obj->getLipsum();
-  $people = $obj->getPeople(); */
+  $title = $obj->title;
+  $lipsum = $obj->lipsum;
+  $people = $obj->people;
+  $personToSubmit = $obj->getPersonToSubmit($_POST) */
 
 $title = 'Interview test';
 $people = array(
@@ -39,7 +52,7 @@ $lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus in
 //I moved the if statement from html to here. I am not sure it's good solution. but for me it looks better
 $personToSubmit = 'Imput the data!';
 if ($_POST) {
-    if (!empty(implode($_POST))) {
+    if (!empty(implode($_POST))) {//I could check all the keys of the array.
         $personToSubmit = implode(' ', $_POST);
     }
 }
@@ -57,6 +70,7 @@ if ($_POST) {
     <body>
 
         <h1><?php echo $title; ?></h1>
+
         <?php
         for ($i = 0; $i < 10; $i++) {
             echo '<p>' . $lipsum . '</p>';
@@ -64,7 +78,7 @@ if ($_POST) {
         ?>
 
         <hr>
-        <form method="post" action=""><!--I removed $_SERVER['REQUEST_URI']. we do not need it here and it's not safe-->
+        <form method="post" action=""><!--I removed $_SERVER['REQUEST_URI'].we do not need it here and it's not safe-->
             <p><label for="firstName">First name</label> <input type="text" name="first_name" id="firstName"></p>
             <p><label for="lastName">Last name</label> <input type="text" name="last_name" id="lastName"></p>
             <p><label for="email">Email</label> <input type="text" name="email" id="email"></p>
@@ -82,13 +96,13 @@ if ($_POST) {
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($people as $person){ ?>
+                <?php foreach ($people as $person) { ?>
                     <tr>
                         <td><?php echo $person['first_name']; ?></td>
                         <td><?php echo $person['last_name']; ?></td>
                         <td><?php echo $person['email']; ?></td>
                     </tr>
-                <?php }; ?>
+                <?php } ?>
             </tbody>
         </table>
 
